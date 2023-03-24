71668ee2e138ab92f73f5956ba5da9d9fe2a5bef
Julius <juliusarendt@icloud.com>
2023-03-24 15:35:48 +0100

    Added Spare-Test for FrameFactoryTest.java

--- src/test/java/FrameFactoryTest.java
+++ src/test/java/FrameFactoryTest.java
@@ -1,2 +1,23 @@
+import frames.FrameFactory;
+import frames.Spare;
+import frames.Strike;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class FrameFactoryTest {
+    @ParameterizedTest
+    @ValueSource(strings = {"-/","1/","2/","3/","4/","5/","6/","7/","8/","9/"})
+    void testForSpare(String frameString) {
+        assertTrue(FrameFactory.getSpecificFrame(frameString) instanceof Spare);
+    }
+
+    @Test
+    void testForStrike() {
+        assertTrue(FrameFactory.getSpecificFrame("X") instanceof Strike);
+    }
+
+
 }

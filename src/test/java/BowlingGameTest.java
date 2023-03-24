6ef5b3d65f3d8ec3294d53593939f51af4a52c09
Julius <juliusarendt@icloud.com>
2023-03-24 13:42:17 +0100

    Added first Test for BowlingGame.java

--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -1,5 +1,22 @@
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class BowlingGameTest {
+    private BowlingGame bowlingGame;
+    @BeforeEach
+    public void setUp() {
+        this.bowlingGame = new BowlingGame();
+    }
 
+    @AfterEach
+    public void tearDown() {
+        this.bowlingGame = null;
+    }
+    @ParameterizedTest
+    @ValueSource(strings = {"X X X X X X X X X X X X"})
+    void testOnlyStrikes(String current){
+        assertEquals(300,bowlingGame.getScore(current));
+    }
 }

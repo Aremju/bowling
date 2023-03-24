24cb7e801f4426b6ede6ab0e819d91d13d43991f
Julius <juliusarendt@icloud.com>
2023-03-24 18:20:57 +0100

    Added Test for Normal-Frame, added Javadoc

--- src/test/java/FrameFactoryTest.java
+++ src/test/java/FrameFactoryTest.java
@@ -1,4 +1,5 @@
 import frames.FrameFactory;
+import frames.Normal;
 import frames.Spare;
 import frames.Strike;
 import org.junit.jupiter.api.Test;
@@ -8,16 +9,36 @@ import org.junit.jupiter.params.provider.ValueSource;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class FrameFactoryTest {
+    /**
+     * Tests if the FrameFactory creates a Spare-Instance out of a
+     * Spare-String
+     *
+     * @param frameString a String which is supposed to be a Spare
+     */
     @ParameterizedTest
     @ValueSource(strings = {"-/","1/","2/","3/","4/","5/","6/","7/","8/","9/"})
     void testForSpare(String frameString) {
         assertTrue(FrameFactory.getSpecificFrame(frameString) instanceof Spare);
     }
 
+    /**
+     * Tests if the FrameFactory creates an Strike instance out of an
+     * X.
+     */
     @Test
     void testForStrike() {
         assertTrue(FrameFactory.getSpecificFrame("X") instanceof Strike);
     }
 
-
+    /**
+     * Tests if the FrameFactory creates instances of Normal-Type with the
+     * following parameters
+     *
+     * @param frameString a String which is supposed to work as a Normal-Frame
+     */
+    @ParameterizedTest
+    @ValueSource(strings = {"12", "34", "1-", "45", "45", "12", "42", "45", "22", "45", "12", "43", "--"})
+    void testforNormal(String frameString) {
+        assertTrue(FrameFactory.getSpecificFrame(frameString) instanceof Normal);
+    }
 }

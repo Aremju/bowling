68a525dcf6b11395203dce486dad0f9a0c888345
Julius <juliusarendt@icloud.com>
2023-03-25 15:44:14 +0100

    Added missing JavaDoc

--- src/main/java/frames/Frame.java
+++ src/main/java/frames/Frame.java
@@ -2,30 +2,59 @@ package frames;
 
 import java.util.List;
 
+/**
+ * This class represents a frame, it consists only
+ * of the current thrown pins by the player and
+ * also the startidx of the player's list
+ */
 public abstract class Frame {
-    private int startIdx;
-    private List<Integer> throwings;
+    private final int startIdx;
+    private final List<Integer> throwings;
 
+    /**
+     * Creates a Frame-instance
+     *
+     * @param throwings currently thrown pins by player
+     */
     public Frame(List<Integer> throwings) {
         this.throwings = throwings;
         this.startIdx = this.throwings.size();
     }
 
+    /**
+     * @return the start idx of the player's list
+     */
     protected int getStartIdx() {
         return this.startIdx;
     }
 
+    /**
+     * @return the list of thrown pins
+     */
     public List<Integer> getThrowings() {
         return this.throwings;
     }
 
+    /**
+     * @return the score of this frame
+     */
     public abstract int getScoreOfFrame();
+
+    /**
+     * @return the frame size (either 1 or 2)
+     */
     public abstract int getFrameSize();
 
+    /**
+     * @return returns the next first throw after this frame
+     */
     public int getNextFirstThrow() {
         return 0;
     }
 
+    /**
+     * @return returns the next first throw after this frame
+     */
     public int getNextSecondThrow() {
         return 0;
     }

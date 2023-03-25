0697edbb4a90d6eb4edbeaeb7feb26b2bfd3267f
Julius <juliusarendt@icloud.com>
2023-03-25 15:46:43 +0100

    Implemented Strike

--- src/main/java/frames/Strike.java
+++ src/main/java/frames/Strike.java
@@ -2,15 +2,24 @@ package frames;
 
 import java.util.List;
 
+/**
+ * This class represents a Strike-instance which is
+ * in points 10 + firstNextThrow + secondNextThrow
+ */
 public class Strike extends Frame {
 
+    /**
+     * creates a Strike-instance and adds 10 to our throwings
+     * @param throwings the current Throws of the player
+     */
     public Strike(List<Integer> throwings) {
         super(throwings);
+        getThrowings().add(10);
     }
 
     @Override
     public int getScoreOfFrame() {
-        return 0;
+        return 10 + getNextFirstThrow() + getNextSecondThrow();
     }
 
     @Override

d50104066c83725c554d55ebd0cb5ab7b0a2f377
Julius <juliusarendt@icloud.com>
2023-03-25 03:35:56 +0100

    Added test cases to BowlingGameTest

--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -2,26 +2,40 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.ValueSource;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+/**
+ * This class represents our main test class
+ * which provides several test cases for the
+ * bowling kata.
+ */
 public class BowlingGameTest {
+    //Bowling instance for each test case
     private BowlingGame bowlingGame;
+
+    /**
+     * Sets up a new BowlingGame instance
+     * for each test case
+     */
     @BeforeEach
     void setUp() {
         this.bowlingGame = new BowlingGame();
     }
 
+    /**
+     * Deletes the BowlingGame instance after
+     * each test
+     */
     @AfterEach
     void tearDown() {
         this.bowlingGame = null;
     }
     @ParameterizedTest
-    @CsvSource({"0,X X X X X X X X X X X X"})
-    void testOnlyStrikes(int result, String currentFrameString){
+    @CsvSource({
+            "300,X X X X X X X X X X X X",
+            "90,9- 9- 9- 9- 9- 9- 9- 9- 9- 9-"
+    })
+    void testBowling(int result, String currentFrameString){
         assertEquals(result,bowlingGame.getScore(currentFrameString));
     }
-
-
 }

aeb735ec4e8374a97db6ab2af49cbbc87034ff26
Julius <juliusarendt@icloud.com>
2023-03-26 04:53:56 +0200

    Finished TestSuite

--- src/test/java/TestSuite.java
+++ src/test/java/TestSuite.java
@@ -1,2 +1,15 @@
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.runner.RunWith;
+
+@RunWith(JUnitPlatform.class)
+@SelectClasses({
+        BonusTest.class,
+        BowlingGameTest.class,
+        FrameFactoryTest.class,
+        NormalTest.class,
+        SpareTest.class,
+        StrikeTest.class
+})
 public class TestSuite {
 }
